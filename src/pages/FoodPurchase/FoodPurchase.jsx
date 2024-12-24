a79daa5e4f25706c9f2e315a37864d008655915c
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-24 23:21:26 +0600

    Updated food purchase

--- src/pages/FoodPurchase/FoodPurchase.jsx
+++ src/pages/FoodPurchase/FoodPurchase.jsx
@@ -64,9 +64,9 @@ const FoodPurchase = () => {
 
     const handleQuantityChange = (e) => {
         const val = parseInt(e.target.value);
-        if (val > food.quantity) {
+        if (val > totaAvailable) {
             toast.error("Stock limit reached.");
-            e.target.value = food.quantity;
+            e.target.value = totaAvailable;
         }
     };
 
@@ -94,116 +94,111 @@ const FoodPurchase = () => {
 
         console.log(purchasedFood);
 
-        const res = await axiosSecure.post("/purchase-food", purchasedFood);
-        console.log(res);
+        // const res = await axiosSecure.post("/purchase-food", purchasedFood);
+        // console.log(res);
 
-        const response = await axiosSecure.patch(`/food/${id}`, {
-            remaining,
-            purchaseQuantity,
-        });
+        // const response = await axiosSecure.patch(`/food/${id}`, {
+        //     remaining,
+        //     purchaseQuantity,
+        // });
 
         setTotalAvailable(remaining);
         setTotalPurchase(purchaseQuantity);
 
-        console.log("remain", response);
+        console.log(remaining, purchaseQuantity);
     };
 
     return (
         <>
             <Banner title={"Food Purchase"} img={bgImg1}></Banner>
 
-            <div className="flex flex-col md:flex-row items-center p-8 bg-gray-200 dark:bg-neutral-800 shadow-md rounded-lg max-w-7xl mx-auto my-12 ">
-                {/* Image Section */}
-                <div className="md:w-1/2 w-full">
-                    <img
-                        src={
-                            food.image ||
-                            "https://i.ibb.co.com/JpsfcNm/louis-hansel-xx-Ic-EAh-It-J0-unsplash.webp"
-                        }
-                        alt="Fruit Smoothie"
-                        className="rounded-lg w-full"
-                    />
-                </div>
-
-                {/* Product Info Section */}
-                <div className="md:w-1/2 w-full md:ml-8 mt-6 md:mt-0">
-                    <h1 className="text-3xl font-bold mb-2">{food.name}</h1>
-                    <div className="flex items-center mb-4">
-                        <span className="text-gray-600 dark:text-gray-400 font-medium">
-                            {food.ownerName}
-                        </span>
-                        <span className="ml-2 text-gray-600 dark:text-gray-400 font-medium">
-                            ({food.ownerEmail})
-                        </span>
-                    </div>
-                    <p className="text-gray-600 dark:text-gray-400 mb-2">
-                        Total Purchased:
-                        <span className="text-neutral-800 dark:text-neutral-200 ml-1 font-medium">
-                            {totalPurchase || 0}
-                        </span>
-                    </p>
-                    <p className="text-xl font-semibold mb-2">
-                        Price:
-                        <span className="text-neutral-800 dark:text-neutral-200 ml-1 font-medium">
-                            ${food.price}
-                        </span>
-                    </p>
-                    <p className="mb-2 text-gray-600 dark:text-gray-400">
-                        Available:
-                        <span className="text-neutral-800 dark:text-neutral-200 font-medium ml-1">
-                            {totaAvailable} pcs
-                        </span>
-                    </p>
-                    <p className="mb-2 text-gray-600 dark:text-gray-400">
-                        Origin:
-                        <span className="text-neutral-800 dark:text-neutral-200 font-medium">
-                            {food.origin}
-                        </span>
-                    </p>
-                    <p className="mb-6 text-gray-600 dark:text-gray-400">
-                        Category:
-                        <span className="text-neutral-800 dark:text-neutral-200 ml-1 font-medium">
-                            {food.category}
-                        </span>
-                    </p>
-                    <div className="text-gray-600 dark:text-gray-400 mb-5">
-                        <span className="mb-2">Ingredients: </span>
-                        <ul className="flex flex-wrap gap-2 w-4/5 text-neutral-800 dark:text-neutral-200 font-medium">
-                            {food?.desc &&
-                                food?.desc.map((item, idx) => (
-                                    <li key={idx}>
-                                        {idx + 1}. {item}
-                                    </li>
-                                ))}
-                        </ul>
+            <div className="bg-gray-200 dark:bg-neutral-800 max-w-2xl mx-auto p-8 rounded-lg my-12">
+                <h2 className="text-3xl text-center font-semibold mb-6">
+                    Food Purchase
+                </h2>
+                <form onSubmit={handlePurchase} className="grid grid-cols-1 gap-4 ">
+                    <div className="space-y-2">
+                        <label className="form-title" htmlFor="name">
+                            name:
+                        </label>
+                        <input
+                            className="form-input"
+                            defaultValue={food.name}
+                            disabled
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Enter food name"
+                        />
                     </div>
 
-                    {/* Size and Quantity */}
-                    <form
-                        onSubmit={handlePurchase}
-                        className="flex items-end gap-3"
-                    >
-                        <div className="space-y-1 w-[calc(100%-7.8225rem)]">
-                            <label className="form-title" htmlFor="quantity">
-                                quantity:
-                            </label>
-                            <input
-                                className="form-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none "
-                                type="number"
-                                onWheel={(e) => e.target.blur()}
-                                min={1}
-                                max={totaAvailable}
-                                onChange={handleQuantityChange}
-                                name="quantity"
-                                id="quantity"
-                                placeholder="Enter food quantity"
-                            />
-                        </div>
-                        <div>
-                            <button className="btn w-full">Purchase</button>
-                        </div>
-                    </form>
-                </div>
+                    <div className="space-y-2">
+                        <label className="form-title" htmlFor="price">
+                            price:
+                        </label>
+                        <input
+                            className="form-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                            defaultValue={food.price}
+                            type="number"
+                            disabled
+                            onWheel={(e) => e.target.blur()}
+                            name="price"
+                            id="price"
+                            placeholder="Enter food price"
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <label className="form-title" htmlFor="quantity">
+                            quantity:
+                        </label>
+                        <input
+                            className="form-input [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+                            min={1}
+                            max={totaAvailable}
+                            onChange={handleQuantityChange}
+                            type="number"
+                            onWheel={(e) => e.target.blur()}
+                            name="quantity"
+                            id="quantity"
+                            placeholder="Enter food quantity"
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <label className="form-title" htmlFor="buyerName">
+                            buyer name:
+                        </label>
+                        <input
+                            className="form-input !text-slate-500"
+                            defaultValue={user?.displayName}
+                            disabled
+                            type="text"
+                            name="buyerName"
+                            id="buyerName"
+                            placeholder="Enter food buyer name"
+                        />
+                    </div>
+
+                    <div className="space-y-2">
+                        <label className="form-title" htmlFor="buyerEmail">
+                            buyer email:
+                        </label>
+                        <input
+                            className="form-input !text-slate-500"
+                            defaultValue={user?.email}
+                            disabled
+                            type="text"
+                            name="buyerEmail"
+                            id="buyerEmail"
+                            placeholder="Enter food buyer email"
+                        />
+                    </div>
+
+                    <div className="col-span-full mt-4">
+                        <button className="btn w-full">Purchase</button>
+                    </div>
+                </form>
             </div>
         </>
     );
