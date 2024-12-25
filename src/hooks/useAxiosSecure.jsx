1f723a04685625501179cb57a18516c9f7faaba9
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-25 16:31:14 +0600

    Implemented axios interceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,32 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import toast from "react-hot-toast";
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
+});
 
 const useAxiosSecure = () => {
-    const axiosInstance = axios.create({
-        baseURL: "http://localhost:5000",
-        withCredentials: true
-    });
+    const { logoutUser } = useAuth() || {};
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(
+            (response) => {
+                return response;
+            },
+            (error) => {
+                if (error.status === 401 || error.status === 403) {
+                    logoutUser().then(() => {
+                        toast.error("You've logged out");
+                    });
+                }
+
+                return Promise.reject(error);
+            }
+        );
+    }, []);
 
     return axiosInstance;
 };
