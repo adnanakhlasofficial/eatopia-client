e3d8312105e4275812ee677639a5385913d6aa9e
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2024-12-26 19:39:17 +0600

    Base url updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 import toast from "react-hot-toast";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://eatopia-server.vercel.app",
     withCredentials: true,
 });
 
