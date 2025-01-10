a7e99abbb8a774ed4c78671789b703999b7ddb12
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-01-10 20:23:08 +0600

    navbar sticky

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -4,20 +4,20 @@ import Footer from "../components/Footer/Footer";
 import { Toaster } from "react-hot-toast";
 
 const Root = () => {
-    return (
-        <div>
-            <header className="bg-gray-200 dark:bg-neutral-900">
-                <Navbar />
-            </header>
-            <main className="min-h-[calc(100vh-24.75rem)]">
-                <Outlet></Outlet>
-            </main>
-            <footer className="bg-gray-200 dark:bg-neutral-900">
-                <Footer />
-            </footer>
-            <Toaster />
-        </div>
-    );
+  return (
+    <div>
+      <header className="bg-gray-200/30 dark:bg-neutral-900/30 backdrop-blur-lg sticky top-0 z-20">
+        <Navbar />
+      </header>
+      <main className="min-h-[calc(100vh-24.75rem)]">
+        <Outlet></Outlet>
+      </main>
+      <footer className="bg-gray-200 dark:bg-neutral-900">
+        <Footer />
+      </footer>
+      <Toaster />
+    </div>
+  );
 };
 
 export default Root;
