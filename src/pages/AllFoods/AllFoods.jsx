92b0832370d66c420433dd39f954c466be72aa24
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-01-10 19:38:32 +0600

    All foods page & My orders page updated

--- src/pages/AllFoods/AllFoods.jsx
+++ src/pages/AllFoods/AllFoods.jsx
@@ -8,118 +8,146 @@ import { useQuery } from "@tanstack/react-query";
 import { ClipLoader } from "react-spinners";
 
 const AllFoods = () => {
-    const axiosSecure = useAxiosSecure();
-    const [foods, setFoods] = useState([]);
-    const [count, setCount] = useState(0);
-    const [currentPage, setCurrentPage] = useState(0);
-    const foodsPerPage = 9;
-    const totalPages = Math.ceil(count / foodsPerPage);
-    const pages = [...Array(totalPages).keys()];
+  const axiosSecure = useAxiosSecure();
+  const [foods, setFoods] = useState([]);
+  const [sortMethod, setSortMethod] = useState("ASN");
+  const [count, setCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const foodsPerPage = 9;
+  const totalPages = Math.ceil(count / foodsPerPage);
+  const pages = [...Array(totalPages).keys()];
 
-    useEffect(() => {
-        axiosSecure.get("/count").then((res) => setCount(res.data.count));
-    }, []);
+  useEffect(() => {
+    axiosSecure.get("/count").then((res) => setCount(res.data.count));
+  }, []);
 
-    const { data, isPending, isError, error } = useQuery({
-        queryKey: ["food", currentPage],
-        queryFn: async () => {
-            const { data } = await axiosSecure.get(
-                `/foods?page=${currentPage}&size=${foodsPerPage}`
-            );
-            setFoods(data.result);
-            return data.result;
-        },
-    });
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["food", currentPage],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(
+        `/foods?page=${currentPage}&size=${foodsPerPage}`
+      );
+      setFoods(data.result);
+      return data.result;
+    },
+  });
 
-    if (isPending) {
-        return (
-            <div className="h-[calc(100vh-25rem)] flex justify-center items-center">
-                <ClipLoader
-                    color={"#3B82F6"}
-                    loading={true}
-                    size={250}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                />
-            </div>
-        );
-    }
-
-    if (isError) {
-        return <p>{error.message}</p>;
-    }
-
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        const value = e.target.search.value;
-        const { data } = await axiosSecure.get(`/food?search=${value}`);
-        setFoods(data);
-    };
-
-    const handleReset = () => {
-        axiosSecure
-            .get(`/foods?page=${currentPage}&size=${foodsPerPage}`)
-            .then((res) => {
-                setFoods(res.data.result);
-            });
-    };
-
+  if (isPending) {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>All Foods | EATOPIA</title>
-                <link rel="canonical" href="https://www.tacobell.com/" />
-            </Helmet>
-            <div>
-                <Banner title={"All Foods"} img={allFoods}></Banner>
-
-                <div className="wrapper my-12 grid place-items-center">
-                    <form
-                        onSubmit={handleSearch}
-                        className="flex flex-col lg:flex-row gap-4 lg:w-1/2"
-                    >
-                        <label htmlFor="search" className="relative w-full">
-                            <input
-                                className="form-input w-full block !pr-28 !bg-blue-100 shadow-lg"
-                                type="text"
-                                name="search"
-                                id="search"
-                                placeholder="Search your favorite food..."
-                            />
-                            <button className="btn absolute top-1/2 -translate-y-1/2 right-0 scale-90">
-                                Search
-                            </button>
-                        </label>
-                        <input
-                            onClick={handleReset}
-                            className="btn w-max mx-auto scale-90 cursor-pointer"
-                            type="reset"
-                        />
-                    </form>
-                </div>
-
-                <div className="wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 gap-6">
-                    {foods.map((food) => (
-                        <FoodCard key={food._id} food={food}></FoodCard>
-                    ))}
-                </div>
-
-                <div className="flex gap-2 w-max mx-auto my-8">
-                    {count > 9 && pages.map((page, idx) => (
-                        <button
-                            onClick={() => setCurrentPage(page)}
-                            className={`btn w-8 flex justify-center items-center ${
-                                currentPage === page ? "!bg-slate-600" : undefined
-                            }`}
-                            key={idx}
-                        >
-                            {idx + 1}
-                        </button>
-                    ))}
-                </div>
-            </div>
-        </HelmetProvider>
+      <div className="h-[calc(100vh-25rem)] flex justify-center items-center">
+        <ClipLoader
+          color={"#3B82F6"}
+          loading={true}
+          size={250}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
     );
+  }
+
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const value = e.target.search.value;
+    const { data } = await axiosSecure.get(`/food?search=${value}`);
+    setFoods(data);
+  };
+
+  const handleReset = () => {
+    axiosSecure
+      .get(`/foods?page=${currentPage}&size=${foodsPerPage}`)
+      .then((res) => {
+        setFoods(res.data.result);
+      });
+  };
+
+  const handleSort = () => {
+    console.log(sortMethod);
+    const sortedFoods = foods.sort((prev, curr) => {
+      if (sortMethod === "ASN") {
+        return prev.price - curr.price;
+      } else {
+        return curr.price - prev.price;
+      }
+    });
+    setFoods([...sortedFoods]);
+  };
+
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>All Foods | EATOPIA</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
+      <div>
+        <Banner title={"All Foods"} img={allFoods}></Banner>
+
+        <div className="wrapper my-12 grid place-items-center">
+          <form
+            onSubmit={handleSearch}
+            className="flex flex-col lg:flex-row gap-4 lg:w-1/2"
+          >
+            <label htmlFor="search" className="relative w-full">
+              <input
+                className="form-input w-full block !pr-28 !bg-blue-100 shadow-lg"
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search your favorite food..."
+              />
+              <button className="btn absolute top-1/2 -translate-y-1/2 right-0 scale-90">
+                Search
+              </button>
+            </label>
+            <input
+              onClick={handleReset}
+              className="btn w-max mx-auto scale-90 cursor-pointer"
+              type="reset"
+            />
+          </form>
+        </div>
+
+        <div className="wrapper flex justify-end gap-4">
+          <select
+            defaultValue={sortMethod}
+            className="form-input !w-max !pr-2"
+            onChange={(e) => setSortMethod(e.target.value)}
+          >
+            <option value={"ASN"}>ASN</option>
+            <option value={"DSN"}>DSN</option>
+          </select>
+          <button onClick={handleSort} className="btn">
+            Sort By Price
+          </button>
+        </div>
+
+        <div className="wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-12 gap-6">
+          {foods.map((food) => (
+            <FoodCard key={food._id} food={food}></FoodCard>
+          ))}
+        </div>
+
+        <div className="flex gap-2 w-max mx-auto my-8">
+          {count > 9 &&
+            pages.map((page, idx) => (
+              <button
+                onClick={() => setCurrentPage(page)}
+                className={`btn w-8 flex justify-center items-center ${
+                  currentPage === page ? "!bg-slate-600" : undefined
+                }`}
+                key={idx}
+              >
+                {idx + 1}
+              </button>
+            ))}
+        </div>
+      </div>
+    </HelmetProvider>
+  );
 };
 
 export default AllFoods;
--- src/pages/MyFood/MyFood.jsx
+++ src/pages/MyFood/MyFood.jsx
@@ -7,57 +7,101 @@ import { AuthContext } from "../../Provider/AuthProvider";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Banner from "../../components/Banner/Banner";
 import allFoods from "../../assets/images/all-foods.jpeg";
+import { Link } from "react-router-dom";
 
 const MyFood = () => {
-    const { user } = useContext(AuthContext);
-    const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
 
-    const { isPending, isError, data, error } = useQuery({
-        queryKey: ["my-foods"],
-        queryFn: async () => {
-            const { data } = await axiosSecure.get(
-                `/my-foods?email=${user.email}`
-            );
-            return data;
-        },
-    });
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["my-foods"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/my-foods?email=${user.email}`);
+      return data;
+    },
+  });
 
-    if (isPending) {
-        return (
-            <div className="h-[calc(100vh-25rem)] flex justify-center items-center">
-                <ClipLoader
-                    color={"#3B82F6"}
-                    loading={true}
-                    size={250}
-                    aria-label="Loading Spinner"
-                    data-testid="loader"
-                />
-            </div>
-        );
-    }
-
-    if (isError) {
-        return <p>{error.message}</p>;
-    }
-
+  if (isPending) {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>My Foods | EATOPIA</title>
-                <link rel="canonical" href="https://www.tacobell.com/" />
-            </Helmet>
-
-            <div>
-                <Banner title={"My Foods"} img={allFoods}></Banner>
-                    {data.length === 0 && <h2 className="text-center text-2xl font-semibold my-20 bg-gray-100 dark:bg-zinc-900 p-3 w-max mx-auto rounded-lg">You haven’t added any food yet!</h2>}
-                <div className="wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 gap-6">
-                    {data.map((food) => (
-                        <FoodCard key={food._id} food={food}></FoodCard>
-                    ))}
-                </div>
-            </div>
-        </HelmetProvider>
+      <div className="h-[calc(100vh-25rem)] flex justify-center items-center">
+        <ClipLoader
+          color={"#3B82F6"}
+          loading={true}
+          size={250}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
     );
+  }
+
+  if (isError) {
+    return <p>{error.message}</p>;
+  }
+  console.log(data);
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>My Foods | EATOPIA</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
+
+      <div>
+        <Banner title={"My Foods"} img={allFoods}></Banner>
+        {data.length === 0 && (
+          <h2 className="text-center text-2xl font-semibold my-20 bg-gray-100 dark:bg-zinc-900 p-3 w-max mx-auto rounded-lg">
+            You haven’t added any food yet!
+          </h2>
+        )}
+        <div className="wrapper my-12 w-full overflow-x-auto">
+          <table className="text-gray-600 dark:text-gray-400 mx-auto text-sm bg bg-gray-100 dark:bg-zinc-900 rounded-md text-left">
+            <thead>
+              <tr className="dark:border-slate-600 text-gray-600 dark:text-gray-400 *:px-4 *:py-2 *:font-semibold">
+                <th></th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length < 1 ? (
+                <tr>
+                  <td
+                    className="text-center text-2xl font-semibold px-4 py-2"
+                    colSpan={6}
+                  >
+                    No food has been ordered yet.
+                  </td>
+                </tr>
+              ) : (
+                data.map((item, idx) => (
+                  <tr
+                    className="border-y border-gray-400 dark:border-slate-600 text-gray-600 dark:text-gray-200 *:px-4 *:py-2"
+                    key={item._id}
+                  >
+                    <td className="text-right">{idx + 1}</td>
+                    <td>{item.name}</td>
+                    <td className="text-right">${item.price}</td>
+                    <td className="text-right">{item.quantity}</td>
+                    <td>
+                      <Link
+                        className="text-blue-600 font-semibold hover:underline underline-offset-2"
+                        to={`/food/update/${item._id}`}
+                      >
+                        Update
+                      </Link>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </HelmetProvider>
+  );
 };
 
 export default MyFood;
